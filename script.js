c209c87dc345939800e277c5c161a244a0a8fa13
Jeston Lewis <jeston@jestonlewiscreative.com>
2024-02-26 18:07:04 -0700

    built operation function

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1/num2;
 }
+
+function operation(num1, op, num2) {
+    switch (op) {
+        case add:
+            return add(num1, num2);
+        case subtract:
+            return subtract(num1, num2);
+        case multiply:
+            return multiply(num1, num2);
+        case divide:
+            return divide(num1, num2);
+    }
+}
