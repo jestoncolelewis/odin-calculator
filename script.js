3016e7917a8f169a980fa038e3ad69ae4eabcba6
Jeston Lewis <jeston@jestonlewiscreative.com>
2024-02-26 18:16:40 -0700

    name update

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function divide(num1, num2) {
     return num1/num2;
 }
 
-function operation(num1, op, num2) {
+function operate(num1, op, num2) {
     switch (op) {
         case add:
             return add(num1, num2);
