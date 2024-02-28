d2811a293c0525dbd2911e2d508e3bc17e9d760e
Jeston Lewis <jeston@jestonlewiscreative.com>
2024-02-28 15:22:10 -0700

    updated operate function to display results

--- script.js
+++ script.js
@@ -16,14 +16,18 @@ function divide(num1, num2) {
 
 function operate(num1, op, num2) {
     switch (op) {
-        case add:
-            return add(num1, num2);
-        case subtract:
-            return subtract(num1, num2);
-        case multiply:
-            return multiply(num1, num2);
-        case divide:
-            return divide(num1, num2);
+        case 'add':
+            to_display(add(num1, num2));
+            break;
+        case 'subtract':
+            to_display(subtract(num1, num2));
+            break;
+        case 'multiply':
+            to_display(multiply(num1, num2));
+            break;
+        case 'divide':
+            to_display(divide(num1, num2));
+            break;
     }
 }
 
